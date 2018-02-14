b6f1034c12fbe5e0cbdc23afc0d3669add9718f8
Dan Boykis <danboykis@gmail.com>
2018-02-14 11:26:34 -0500

    upgrade to clojure 1.9
    version upgrade to 0.1.2

--- project.clj
+++ project.clj
@@ -1,14 +1,14 @@
-(defproject radigost "0.1.1"
+(defproject radigost "0.1.2-SNAPSHOT"
   :description "Work with JWT in Clojure"
   :url "https://github.com/danboykis/radigost"
   :license {:name "Unlicense" :url "http://unlicense.org/"}
   :source-paths ["src/clojure"]
   :java-source-paths ["src/java"]
-  :dependencies [[org.clojure/clojure "1.9.0-alpha15"]]
+  :dependencies [[org.clojure/clojure "1.9.0"]]
 
   :profiles {:dev {:source-paths ["dev" "test/resources"]
                    :repl-options {:init-ns user}
                    :dependencies [[cheshire "5.6.2"]
-                                  [http-kit "2.1.18"]
+                                  [http-kit "2.2.0"]
                                   [org.clojure/test.check "0.9.0"]]}})
 
