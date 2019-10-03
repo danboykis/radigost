51ddb5ba5c4d020aac771cfc670f29c3d4668550
Dan Boykis <danboykis@gmail.com>
2019-10-03 13:49:15 -0400

    update deps

--- project.clj
+++ project.clj
@@ -4,11 +4,11 @@
   :license {:name "Unlicense" :url "http://unlicense.org/"}
   :source-paths ["src/clojure"]
   :java-source-paths ["src/java"]
-  :dependencies [[org.clojure/clojure "1.9.0"]]
+  :dependencies [[org.clojure/clojure "1.10.1"]]
 
   :profiles {:dev {:source-paths ["dev" "test/resources"]
                    :repl-options {:init-ns user}
-                   :dependencies [[cheshire "5.6.2"]
-                                  [http-kit "2.2.0"]
+                   :dependencies [[cheshire "5.9.0"]
+                                  [http-kit "2.3.0"]
                                   [org.clojure/test.check "0.9.0"]]}})
 
